47cdc335eb8033c3b0f4760bb04805dea87dcc06
laboranto <justavojo@gmail.com>
2022-09-11 03:02:51 +0900

    modified:   node_modules/.cache/.eslintcache 	modified:   node_modules/.cache/default-development/0.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/logo.svg 	src/icons/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="notify">
+    <p>GPS 비활성화 됨</p>
+    </div>
+    <div className="clock">
+      <h1>
+      <span>00</span>:
+      <span>00</span>:
+      <span>00</span>
+      </h1>
+      <h3>
+      <span>부산</span>은
+      <span> 구름이 많으며 </span>
+      기온은 <span>25도</span>
+      </h3>
+    </div>
     </div>
   );
+}
+
+function WeatherSymbol() {
+  
+}
+
+function WeatherText() {
+
 }
 
 export default App;
