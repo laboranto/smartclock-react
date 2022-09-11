175e449a64191cd849e8b56f5d5d1c8fbdaa6a60
laboranto <justavojo@gmail.com>
2022-09-11 11:48:28 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  /* 시계 영역 */
+  const [time,setTime] = useState('00:00:00');
+
+  const currentTime = () => {
+    const date = new Date();
+    const hrs = String(date.getHours()).padStart(2,'0');
+    const min = String(date.getMinutes()).padStart(2,'0');
+    const sec = String(date.getSeconds()).padStart(2,'0');
+    setTime(`${hrs}:${min}:${sec}`)
+  }
+
+  const continueTime = () => {
+    setInterval(currentTime,1000);
+  }
+
+  continueTime();
+
+  /* 날씨 영역 */
+  const city = '부산';
+  const sky = '맑으며';
+  const temp = '28';
+
   return (
-    <div>
+    <div className="app">
     <div className="notify">
-    <p>GPS 비활성화 됨</p>
+    <p>🌏 GPS 연결 중</p>
+    </div>
+    <div className="sky-icon">
+    <img src="icons/01d.svg"/>
     </div>
     <div className="clock">
       <h1>
-      <span>00</span>:
-      <span>00</span>:
-      <span>00</span>
+      {time}
       </h1>
-      <h3>
-      <span>부산</span>은
-      <span> 구름이 많으며 </span>
-      기온은 <span>25도</span>
-      </h3>
+      <p>
+      현재 {city}의 날씨는 {sky}, 기온은 섭씨 {temp}도입니다.
+      </p>
     </div>
+    <NewsCast></NewsCast>
     </div>
   );
 }
 
-function WeatherSymbol() {
-  
-}
-
-function WeatherText() {
-
+function NewsCast(){
+return(
+  <div className="newsWrap">
+    <div className="news">
+    <h3>헤드라인</h3>
+    <p>내용</p>
+    </div>
+  </div>
+)
 }
 
 export default App;
